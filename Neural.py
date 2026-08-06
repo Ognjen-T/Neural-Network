61b80ed625f5a31bae0775b84b2aad395c009a0d
Ognjen-T <ognjentairovski8@gmail.com>
2026-08-06 20:55:12 +0200

    v1.1 - Added dataset training with epoch-based learning

--- Neural.py
+++ Neural.py
@@ -1,7 +1,7 @@
 import time
 
-input_value = int(input("Enter a value: "))
-goal = 158641
+input_values = [1, 2, 3, 4, 5]
+goals = [5, 10, 15, 20, 25]
 weight = 0.5
 bias = 0.0
 learning_rate = 0.01
@@ -9,22 +9,32 @@ learning_rate = 0.01
 
 start = time.time()
 for i in range(1000):
-    output = (input_value * weight) + bias
-    error = goal - output
-    loss = (goal - output) ** 2
+    totat_loss = 0
+    for e in range (len(input_values)):
+        output = (input_values[e] * weight) + bias
+        error = goals[e] - output
+        loss = error ** 2
 
-    if loss < 0.0000001:
+        weight += learning_rate * error * input_values[e]
+        
+        bias += learning_rate * error
+
+        totat_loss += loss
+    average_loss = totat_loss/len(input_values)
+
+    if average_loss < 0.0000001:
         break
-
-    weight += learning_rate * error * input_value
-
-    bias += learning_rate * error
     
+
+    if i % 100 == 0:
+        print(f"Epoch {i}, Average Loss: {average_loss:.6f}")
+        
 end = time.time()
+
+test_input = int(input("Enter a test value: "))
+prediction = test_input * weight + bias
 print("")
 print(f"--------------------------------\n Training took \033[91m{end - start:.6f}\033[0m seconds\n--------------------------------")
-print("Input =", input_value)
-print("Output =", output)
-print("Error =", error)
-print("Loss =", loss)
-print("Weight =", weight)
+print("Weight = ", weight)
+print("Bias = ", bias)
+print("Prediction for ", test_input , " is = " , prediction)
