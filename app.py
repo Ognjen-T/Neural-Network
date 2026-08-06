ffd4deed5a99583e60823473a8cb1246887bc42f
Ognjen-T <ognjentairovski8@gmail.com>
2026-08-06 15:35:30 +0200

    Nerual-Network-v1

---
+++ app.py
@@ -0,0 +1,30 @@
+import time
+
+input_value = int(input("Enter a value: "))
+goal = 15
+weight = 0.5
+bias = 0.0
+learning_rate = 0.001
+
+
+start = time.time()
+for i in range(1000):
+    output = (input_value * weight) + bias
+    error = goal - output
+    loss = (goal - output) ** 2
+
+    if loss < 0.0001:
+        break
+
+    weight += learning_rate * error * input_value
+
+    bias += learning_rate * error
+    
+end = time.time()
+print("")
+print(f"----------------\n Training took \033[91m{end - start:.6f}\033[0m seconds\n----------------")
+print("Input =", input_value)
+print("Output =", output)
+print("Error =", error)
+print("Loss =", loss)
+print("Weight =", weight)
